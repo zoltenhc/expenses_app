8514c4fb94c5d1c8ec59706ec99dda52e376e7eb
zoltenhc <zoltenhc@gmail.com>
2022-03-22 15:20:11 +0100

    Initial Commit

---
+++ expenses_app/lib/widgets/adaptive_buttons.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
+
+class AdaptiveButton extends StatelessWidget {
+  final String text;
+  final Function handler;
+
+  AdaptiveButton(this.text, this.handler);
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS ? CupertinoButton(child:  Text(
+      text,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+      ),
+    ), onPressed: handler) : FlatButton(
+        textColor: Theme.of(context).primaryColor,
+        onPressed: handler,
+        child: Text(
+          "Choose Date",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ));
+  }
+}
---
+++ expenses_app/lib/widgets/new_transaction.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/cupertino.dart';
+import 'adaptive_buttons.dart';
+
+class NewTransaction extends StatefulWidget {
+  final Function addTX;
+
+  NewTransaction(this.addTX) ;
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final _titleController = TextEditingController();
+
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
+
+  void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
+      return;
+    }
+    widget.addTX(
+      enteredTitle,
+      enteredAmount,
+      _selectedDate,
+    );
+    Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Card(
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: "Title"),
+                controller: _titleController,
+                onSubmitted: (_) => _submitData(),
+                //onChanged: (val) { titleInput = val},
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Amount"),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitData(),
+                //onChanged:  { titleInput = val},
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(_selectedDate == null
+                          ? "No Date choosen"
+                          : "Picked Date: ${DateFormat.yMd().format(_selectedDate)}"),
+                    ),
+AdaptiveButton("Choose Date", _presentDatePicker)
+                  ],
+                ),
+              ),
+              RaisedButton(
+                onPressed: _submitData,
+                child: Text("Add Transaction"),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).textTheme.button.color,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
